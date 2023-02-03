3df7446661515c89507f6079ce64765ac96935cf
DmitrijDev <karabutde@mail.ru>
2023-02-04 02:14:45 +0400

    ///

--- Chess/Controls/GamePanelButton.cs
+++ Chess/Controls/GamePanelButton.cs
@@ -32,8 +32,8 @@ namespace Chess
         {
             _images = new Bitmap[37];
 
-            var originalImages = new Bitmap[7] {null, new Bitmap("King.jpg"), new Bitmap("Queen.jpg"), new Bitmap("Rook.jpg"),
-            new Bitmap("Knight.jpg"), new Bitmap("Bishop.jpg"), new Bitmap("Pawn.jpg") };
+            var originalImages = new Bitmap[7] {null, new Bitmap("Pictures/King.jpg"), new Bitmap("Pictures/Queen.jpg"), new Bitmap("Pictures/Rook.jpg"),
+            new Bitmap("Pictures/Knight.jpg"), new Bitmap("Pictures/Bishop.jpg"), new Bitmap("Pictures/Pawn.jpg") };
 
             for (var i = 1; i < 37; ++i)
             {
